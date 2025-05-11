4738873b472dd224c7ac8e7f1bc6d12796b89f3c
i-icc <ichi623110@gmail.com>
2025-05-11 19:22:13 +0900

    Fix basePath and assetPrefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/app-icon-generator',
+  assetPrefix: '/app-icon-generator/',
 };
 
 module.exports = nextConfig;
