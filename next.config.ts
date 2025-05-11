1bd080ba95a7a40dfb8f8f5c017d6f3e25e5b2b2
i-icc <ichi623110@gmail.com>
2025-05-11 18:57:33 +0900

    remove export

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  basePath: '/app-icon-generator', // ← リポジトリ名に変更
 };
 
 module.exports = nextConfig;
